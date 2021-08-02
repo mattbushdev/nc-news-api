f5b2be4d41084aa6f384bf4a4bab3784bfc1d6cf
Ammar Hassan <ammar@ammarhassan.co.uk>
2021-08-02 16:16:01 +0100

    insert logic

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,12 @@
 const db = require("../connection.js");
+const format = require("pg-format");
 // const data = require("../data/development-data/index.js");
-
+const {
+  formatTopicsData,
+  formatUsersData,
+  formatArticleData,
+  formatCommentData,
+} = require("../utils/data-manipulation");
 const seed = async ({ articleData, commentData, topicData, userData }) => {
   await db.query(`DROP TABLE IF EXISTS comments;`);
   await db.query(`DROP TABLE IF EXISTS articles;`);
@@ -30,7 +36,7 @@ const seed = async ({ articleData, commentData, topicData, userData }) => {
    votes INT DEFAULT 0,
    topic TEXT REFERENCES topics(slug) NOT NULL,
    author VARCHAR(100) REFERENCES users(username),
-   created_at TIMESTAMP
+   created_at TIMESTAMP DEFAULT NOW()
     );`);
 
   console.log("articles table created");
@@ -40,19 +46,54 @@ const seed = async ({ articleData, commentData, topicData, userData }) => {
     author VARCHAR(20) REFERENCES users(username),
     article_id INT REFERENCES articles(article_id),
     votes INT DEFAULT 0,
-    created_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT NOW(),
     body TEXT NOT NULL
       );`);
 
   console.log("comments table created");
 
-  await db.query(`INSERT INTO topics (slug)`);
+  const topicDataInsert = format(
+    `INSERT INTO topics 
+  (slug, description)
+  VALUES %L RETURNING *;`,
+    formatTopicsData(topicData)
+  );
 
   // 2. insert data
-};
+  const insertData = await db.query(topicDataInsert);
 
-// seed().then((result) => {
-//   console.log("anything here?");
-// });
+  //console.log(insertData.rows);
+
+  const userDataInsert = format(
+    `INSERT INTO users
+    (username, avatar_url, name)
+    VALUES %L RETURNING *;`,
+    formatUsersData(userData)
+  );
+
+  const userInsertData = await db.query(userDataInsert);
+
+  //console.log(userInsertData.rows);
+
+  const articleDataInsert = format(
+    `INSERT INTO articles
+    (title, body, votes, topic, author, created_at)
+    VALUES %L RETURNING *;`,
+    formatArticleData(articleData)
+  );
+
+  const articleInsertData = await db.query(articleDataInsert);
+
+  //COMMENTS
+  const commentsDataInsert = format(
+    `INSERT INTO comments
+    (author, article_id, votes, created_at, body)
+    VALUES %L RETURNING *;`,
+    formatCommentData(commentData, articleInsertData.rows)
+  );
+
+  const commentInsertData = await db.query(commentsDataInsert);
+  console.log(commentInsertData.rows);
+};
 
 module.exports = seed;
